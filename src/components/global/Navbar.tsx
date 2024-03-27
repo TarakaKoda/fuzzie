cfaa7c76a8ed52b1fa6e7a5a8221e30afcaa8ca3
TarakaKoda <taraka.koda@gmail.com>
2024-03-27 17:40:42 +0530

    Build a Navbar component

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,3 +1,4 @@
+import { MenuIcon } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -39,7 +40,21 @@ const Navbar = async (props: Props) => {
           </li>
         </ul>
       </nav>
-      <aside className=""></aside>
+      <aside className="flex items-center gap-4">
+        <Link
+          href="/dashboard"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[2px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+          <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+            {/* // Todo: wire up user */}
+            {true ? "Dashboard" : "Get Started"}
+          </span>
+        </Link>
+        {
+          // Todo: wire up user
+        }
+        <MenuIcon className="md:hidden" />
+      </aside>
     </header>
   );
 };
