ea3cd034136d785340380702fedd13a02474fb9c
TarakaKoda <taraka.koda@gmail.com>
2024-03-28 02:07:05 +0530

    Build a complete landing page with using Aceternity Ui components

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -6,7 +6,7 @@ type Props = {};
 
 const Navbar = async (props: Props) => {
   return (
-    <header className="fixed right-0 left-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
+    <header className="fixed right-0  left-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
         <p className="text-3xl font-bold">Fu</p>
         <Image
