3577f69a06551caca8b500b8caa96e92931a3880
TarakaKoda <taraka.koda@gmail.com>
2024-04-03 00:33:34 +0530

    Implemented a settings page allowing users to update their information. Utilized React Hook Forms for efficient form handling, Zod for data validation, and Shadcn UI & Aceternity UI form components for a sleek UI.

--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import ProfileForm from "@/components/forms/ProfileForm";
 
 const SettingsPage = () => {
+  // Todo: Wire up the profile picture
   return (
-    <div>SettingsPage</div>
-  )
-}
+    <div className="flex flex-col gap-4">
+      <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
+        <span>Settings</span>
+      </h1>
+      <div className="flex flex-col w-full md:items-center gap-10 p-6">
+        <div className="flex flex-col md:items-center">
+          <h2 className="text-2xl font-bold">User Profile</h2>
+          <p className="text-base text-white/50">
+            Add or update your information
+          </p>
+        </div>
+        <ProfileForm />
+      </div>
+    </div>
+  );
+};
 
-export default SettingsPage
+export default SettingsPage;
