1b9574c0e414dfd1fe4231b2fd4b0da0a8ffb556
TarakaKoda <taraka.koda@gmail.com>
2024-04-04 00:38:55 +0530

    Added sign-in/sign-up pages for clerk authentication and created a webhook for create or update user details into database

---
+++ src/app/api/clerk-webhook/route.ts
@@ -0,0 +1,36 @@
+import prisma from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(request: NextRequest) {
+  try {
+    const body = await request.json();
+    const { id, email_addresses, first_name, last_name, image_url } =
+      body?.data;
+    const email = email_addresses[0]?.email_address;
+    console.log(body);
+
+    await prisma.user.upsert({
+      where: { clerkId: id },
+      update: {
+        email,
+        firstName: first_name,
+        lastName: last_name,
+        profileImage: image_url,
+      },
+      create: {
+        clerkId: id,
+        email,
+        firstName: first_name || "",
+        lastName: last_name || "",
+        profileImage: image_url || "",
+      },
+    });
+
+    return new NextResponse("User updated in database successfully", {
+      status: 200,
+    });
+  } catch (error) {
+    console.log("Error updating database", error);
+    return new NextResponse("Error updating user in database", { status: 500 });
+  }
+}
