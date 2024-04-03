12a9634fd750c5a7d6f260fee137f5afae6acc97
TarakaKoda <taraka.koda@gmail.com>
2024-04-03 16:27:55 +0530

    Implemented upload image functionality using uploadCare.

---
+++ src/app/(main)/(pages)/settings/_components/ProfilePicture.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import UploadCareButton from "./UploadCareButton";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { X } from "lucide-react";
+
+type Props = {
+  onDelete?: any;
+  onUpload?: any;
+  userImage: string | null;
+};
+
+const ProfilePicture = ({ onDelete, onUpload, userImage }: Props) => {
+  const router = useRouter();
+  const onRemoveProfileImage = async () => {
+    const response = await onDelete();
+    if (response) {
+      router.refresh();
+    }
+  };
+  return (
+    <div className="flex flex-col">
+      <p className="text-lg text-white">Profile Picture</p>
+      <div className="flex h-[30vh] flex-col items-center justify-center">
+        {userImage ? (
+          <>
+            <div className="relative h-full w-2/12">
+              <Image src={userImage} fill alt="User Profile Picture" />
+            </div>
+            <Button
+              onClick={onRemoveProfileImage}
+              className="bg-transparent text-white/70 hover:bg-transparent hover:text-white">
+              <X /> Remove Logo
+            </Button>
+          </>
+        ) : (
+          <UploadCareButton />
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePicture;
