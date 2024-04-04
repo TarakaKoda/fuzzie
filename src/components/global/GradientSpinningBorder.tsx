4f810bbf4b361081ad43881baadb2782771ac1cc
TarakaKoda <taraka.koda@gmail.com>
2024-04-04 17:14:51 +0530

    Build connections page with custom connection card component

--- src/components/global/GradientSpinningBorder.tsx
+++ src/components/global/GradientSpinningBorder.tsx
@@ -2,11 +2,15 @@ import React from "react";
 
 type Props = {
   children: React.ReactNode;
+  rounded?: string;
 };
 
 const GradientSpinningBorder = (props: Props) => {
   return (
-    <div className="flex overflow-hidden p-[3px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-4 focus:ring-offset-slate-50 !m-0 object-top rounded-full relative transition duration-500 border-1 border dark:border-none">
+    <div
+      className={`flex overflow-hidden p-[3px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-4 focus:ring-offset-slate-50 !m-0 object-top  relative transition duration-500 border-1 border dark:border-none ${
+        props.rounded ? props.rounded : "rounded-full"
+      }`}>
       <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#D200FF_0%,#393BB2_50%,#D200FF_100%)] dark:bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
       {props.children}
     </div>
---
+++ src/provider/connections-provider.tsx
@@ -0,0 +1,46 @@
+'use client'
+
+export type ConnectionProviderProps = {
+  discordNode: {
+    webhookURL: string
+    content: string
+    webhookName: string
+    guildName: string
+  }
+  setDiscordNode: React.Dispatch<React.SetStateAction<any>>
+  googleNode: {}[]
+  setGoogleNode: React.Dispatch<React.SetStateAction<any>>
+  notionNode: {
+    accessToken: string
+    databaseId: string
+    workspaceName: string
+    content: ''
+  }
+  workflowTemplate: {
+    discord?: string
+    notion?: string
+    slack?: string
+  }
+  setNotionNode: React.Dispatch<React.SetStateAction<any>>
+  slackNode: {
+    appId: string
+    authedUserId: string
+    authedUserToken: string
+    slackAccessToken: string
+    botUserId: string
+    teamId: string
+    teamName: string
+    content: string
+  }
+  setSlackNode: React.Dispatch<React.SetStateAction<any>>
+  setWorkFlowTemplate: React.Dispatch<
+    React.SetStateAction<{
+      discord?: string
+      notion?: string
+      slack?: string
+    }>
+  >
+  isLoading: boolean
+  setIsLoading: React.Dispatch<React.SetStateAction<boolean>>
+}
+
