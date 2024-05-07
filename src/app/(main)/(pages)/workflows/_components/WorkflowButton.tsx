314942c17aacece1e4f1d7155102cb0526e366f6
TarakaKoda <taraka.koda@gmail.com>
2024-05-07 19:36:52 +0530

    Implemented Stripe to charge month to month subscriptions and Payments based on credits consumed

--- src/app/(main)/(pages)/workflows/_components/WorkflowButton.tsx
+++ src/app/(main)/(pages)/workflows/_components/WorkflowButton.tsx
@@ -4,6 +4,7 @@ import WorkflowForm from "@/components/forms/WorkflowForm";
 import CustomModal from "@/components/global/CustomModal";
 import GradientSpinningBorder from "@/components/global/GradientSpinningBorder";
 import { Button } from "@/components/ui/button";
+import { useBilling } from "@/provider/billing-provider";
 import { useModal } from "@/provider/modal-provider";
 import { Plus } from "lucide-react";
 import React from "react";
@@ -12,6 +13,7 @@ type Props = {};
 
 const WorkflowButton = (props: Props) => {
   const { setOpen, setClose } = useModal();
+  const { credits } = useBilling();
 
   const handleClick = () => {
     setOpen(
@@ -28,7 +30,13 @@ const WorkflowButton = (props: Props) => {
       <Button
         size={"icon"}
         className="relative rounded-full bg-white hover:bg-background text-black dark:bg-black dark:text-white"
-        onClick={handleClick}>
+        {...(credits !== "0"
+          ? {
+              onClick: handleClick,
+            }
+          : {
+              disabled: true,
+            })}>
         <Plus className="dark:text-white" />
       </Button>
     </GradientSpinningBorder>
